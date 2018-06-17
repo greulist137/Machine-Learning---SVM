379b94724318fd418f8aa07cd81fef400f32b573
Steven Greulich <greulich.steven@gmail.com>
2018-06-17 05:16:53 -0700

    Commit - Creation of initial SVM Classifier
    The purpose of this commit is for the creation of the initial SVM classifier using a  Linear Kernal approach

--- ud120-projects-master/svm/svm_author_id.py
+++ ud120-projects-master/svm/svm_author_id.py
@@ -12,19 +12,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#Create initial SVM with a Linear kernal and fit data to it
+clf = svm.SVC(kernel = 'linear')
+clf.fit(features_train, labels_train)
 
+#use the features_test testing data now for predictions and assign it to a prediction variable
+pred = clf.predict(features_test)
+print('Prediction: ' + pred)
 
+#Calculate the accuracy and present the results
+acc_score = accuracy_score(pred, labels_test)
 
-#########################################################
-### your code goes here ###
-
-#########################################################
-
+print('Accuracy: ' + acc_score) 
 
